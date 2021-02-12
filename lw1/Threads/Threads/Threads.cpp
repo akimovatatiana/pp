269b0c09696812e7d46525160db6630b0766c432
akimovatatiana <akimova.tnya@gmail.com>
2021-02-12 15:52:00 +0300

    Added lw1

---
+++ lw1/Threads/Threads/Threads.cpp
@@ -0,0 +1,34 @@
+﻿#include <windows.h>
+#include <string>
+#include <iostream>
+#include <string>
+
+DWORD WINAPI ThreadProc(CONST LPVOID lpParam)
+{
+    std::string outStr = "Поток №" + std::to_string(int(lpParam)) + " выполняет свою работy\n";
+    std::cout << outStr;
+    ExitThread(0); // функция устанавливает код завершения потока в 0
+}
+
+int main(int argc, char* argv[])
+{
+    setlocale(LC_ALL, "Russian");
+    auto threadCount = atoi(argv[1]);
+
+    // создание  потоков
+    HANDLE* handles = new HANDLE[threadCount];
+    for (size_t i = 0; i < threadCount; ++i) 
+    {
+        handles[i] = CreateThread(NULL, 0, &ThreadProc, LPVOID(i), CREATE_SUSPENDED, NULL);
+    }
+
+    // запуск потоков
+    for (size_t i = 0; i < threadCount; ++i)
+    {
+        ResumeThread(handles[i]);
+    }
+
+    // ожидание окончания работы потоков
+    WaitForMultipleObjects(threadCount, handles, true, INFINITE);
+    return 0;
+}
