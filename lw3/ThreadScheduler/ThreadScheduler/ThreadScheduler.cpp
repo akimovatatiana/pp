de6ee7ed4e4df349755ab04da8509b94a93e13d3
akimovatatiana <akimova.tnya@gmail.com>
2021-03-22 17:31:48 +0300

    Updated lw3

--- lw3/ThreadScheduler/ThreadScheduler/ThreadScheduler.cpp
+++ lw3/ThreadScheduler/ThreadScheduler/ThreadScheduler.cpp
@@ -3,66 +3,59 @@
 #include <iostream>
 #include <string>
 #include <fstream>
-#include <clocale>
-#include <chrono>
+#include <vector>
+#include <ctime>
 
 using namespace std;
 
 const int THREAD_COUNT = 2;
-const int OPERATION_COUNT = 10;
-const string INVALID_ARGS = "Invalid arguments count. Usage: threadschedulaer.exe <file>";
+const int OPERATION_COUNT = 100;
+const string INVALID_ARGS = "Invalid arguments count. Usage: ThreadScheduler.exe <fileForFirstThread> <fileForSecondThread>";
 
 struct DataForProc
 {
     int threadNumber;
-    int time;
+    clock_t time;
     string fileName;
 };
 
 DWORD WINAPI ThreadProc(CONST LPVOID lpParam)
 {
     DataForProc* lpData = static_cast<DataForProc*>(lpParam);
-    int threadNumber = lpData->threadNumber;
-    int startTime = lpData->time;
+    int threadNumber = lpData->threadNumber + 1;
+    clock_t startTime = lpData->time;
     ofstream file;
     file.open(lpData->fileName);
 
     for (size_t i = 0; i < OPERATION_COUNT; i++)
     {
-        int operationStartTime = clock();
-        unsigned long resultTime = startTime - operationStartTime;
-        file << threadNumber << "|" << resultTime << endl;
+        clock_t operationStartTime = clock();
+        clock_t resultTime = operationStartTime - startTime;
+        std::string outStr = to_string(threadNumber) + "|" + to_string(resultTime) +"\n";
+        file << outStr;
     }
-
-    /*std::string outStr = "Поток №" + std::to_string(int(lpParam)) + " выполняет свою работy\n";
-    std::cout << outStr;*/
     ExitThread(0); // функция устанавливает код завершения потока в 0
 }
 
 int main(int argc, char* argv[])
 {
-    //unsigned long startTime = GetTickCount64();
-    int startTime = clock();
+    auto startTime = clock();
     setlocale(LC_ALL, "Russian");
 
-    /*if (argc != 2)
+    if (argc != 3)
     {
         cout << INVALID_ARGS << endl;
         return 1;
-    }*/
-    string file = "input.txt";
-    //ofstream file;
-
-    //file.open(argv[1]);
+    }
+    vector<string> files;
+    files.push_back(argv[1]);
+    files.push_back(argv[2]);
 
     // создание  потоков
     HANDLE* handles = new HANDLE[THREAD_COUNT];
     for (int i = 0; i < THREAD_COUNT; i++)
     {
-        DataForProc* data = new DataForProc{i, startTime, file};
-        /*data->threadNumber = i;
-        data->time = startTime;
-        data->fileName = file;*/
+        DataForProc* data = new DataForProc{i, startTime, files[i]};
         handles[i] = CreateThread(NULL, 0, &ThreadProc, data, CREATE_SUSPENDED, NULL);
     }
 
